cdb30f6d29ebbbb7a45885102544d794775280e7
guoweisj <1511077882@qq.com>
2019-04-22 10:57:48 +0800

    订单管理

---
+++ app/Model/order.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class order extends Model
+{
+    //表名
+
+    protected $table = "jy_order";
+
+    public $timestamps = true;
+}
